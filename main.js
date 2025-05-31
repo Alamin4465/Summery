6cf274802fd72dabb57af59c71e70c4e7e667e3a
Alamin4465 <alaminhosen2779@gmail.com>
2025-06-01 01:42:58 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-/// main.js
-import { submitHandler } from './transaction.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const navSubmit = document.getElementById('nav-submit');
-  if (navSubmit) {
-    navSubmit.addEventListener('click', () => {
-      submitHandler();
-    });
-  }
-});
